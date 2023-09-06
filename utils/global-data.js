409efa5fcea42c9c84b50e17d4ec4cbe26d359f5
Griffin Wirth <70413909+WirthG@users.noreply.github.com>
2023-09-06 18:11:11 -0400

    name

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Griffin Wirth';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'CMPSC-203 Weekly Posts';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
